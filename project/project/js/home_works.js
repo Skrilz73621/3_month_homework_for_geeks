9d45498fcc4f314d963910533bed7a57fb6e1a09
Skrilz73621 <148931591+Skrilz73621@users.noreply.github.com>
2024-08-04 15:28:21 +0600

    Updated home_works.js
    добавил бесконечный квадрат, и секундомер

--- project/project/js/home_works.js
+++ project/project/js/home_works.js
@@ -2,9 +2,49 @@ const gmailInput = document.querySelector('#gmail_input')
 const gmailButton = document.querySelector('#gmail_button')
 const gmailResult = document.querySelector('#gmail_result')
 const redSquare = document.querySelector('.child_block') 
+const parentBlock = document.querySelector('.parent_block')
 let redSquareLeft = 0
+let redSquareBottom = 0
+let parentBlockWidth = parentBlock.offsetWidth
+let parentBlockHeight = parentBlock.offsetHeight
+const reset = document.querySelector('#reset')
+const start = document.querySelector('#start')
+const stopBtn = document.querySelector('#stop')
+const seconds = document.querySelector('#seconds')
+let stopper = false
+let num = 0
 
-const regExp = /[a-z0-9]{10,30}@(mail|gmail).(com|ru)/
+
+
+    start.addEventListener('click', ()=>{
+        if(!stopper){
+            const interval = setInterval(() => {
+                num++
+                seconds.innerHTML = num
+            }, 1000);
+            stopper = true
+            stopBtn.addEventListener('click', ()=>{
+                clearInterval(interval)
+                stopper = false
+            })
+        
+            reset.addEventListener('click', ()=>{
+                clearInterval(interval)
+                num=0
+                seconds.innerHTML = num
+                stopper = false
+            })
+        }
+    
+    })
+
+
+
+
+
+
+
+const regExp = /^[\w\W]{10,30}@(mail|gmail).(com|ru)$/
 
 
 gmailButton.addEventListener('click', ()=>{
@@ -20,12 +60,28 @@ gmailButton.addEventListener('click', ()=>{
 
 
 const moveBlock = ()=>{
-    if(redSquareLeft < 448){
-        redSquareLeft++
+
+    console.log(redSquareLeft, redSquareBottom, parentBlockWidth, parentBlockHeight);
+    if(redSquareLeft < parentBlockWidth - redSquare.offsetWidth && redSquareBottom === 0){
+        redSquareLeft += 5
         redSquare.style.left = `${redSquareLeft}px`
+    }else if(redSquareLeft === parentBlockWidth - redSquare.offsetWidth && redSquareBottom < parentBlockHeight - redSquare.offsetHeight){
+        redSquareBottom += 5
+        redSquare.style.top = `${redSquareBottom}px`
+    }else if(redSquareLeft > 0 && redSquareBottom > 0){
+        redSquareLeft -=5
+        redSquare.style.left = `${redSquareLeft}px`
+    }else if(redSquareLeft === 0 && redSquareBottom > 0){
+        redSquareBottom -= 5
+        redSquare.style.top = `${redSquareBottom}px`
     }
+
+
     requestAnimationFrame(moveBlock)
 }
 
 moveBlock()
+
+
+
 
