f567f26a13656dedd6c2cafad921bec57832187a
Skrilz73621 <148931591+Skrilz73621@users.noreply.github.com>
2024-08-09 20:28:07 +0600

    Update lesson.js

--- project/project/js/lesson.js
+++ project/project/js/lesson.js
@@ -15,3 +15,65 @@ phoneButton.addEventListener('click', ()=>{
             phoneSpan.style.color = 'red'
         }
 })
+
+
+// tab slider
+
+const tabContentBlocks = document.querySelectorAll('.tab_content_block')
+const tabItems = document.querySelectorAll('.tab_content_item')
+const tabParent = document.querySelector('.tab_content_items')
+
+console.log(tabItems)
+
+const hideTabContent = () =>{
+    tabContentBlocks.forEach((item) =>{
+        item.style.display = 'none'
+
+    })
+
+    tabItems.forEach((item) =>{
+        item.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabContent = (index = 0) =>{
+    tabContentBlocks[index].style.display = 'block'
+    tabItems[index].classList.add('tab_content_item_active')
+}
+
+hideTabContent()
+showTabContent()
+
+
+let q = 0
+
+const changeTab = () =>{
+    if(q < 5){
+        hideTabContent()
+        showTabContent(q)
+        q++
+    }else if (q >= 5){
+        q = 0
+        changeTab()
+        console.log('фыросироф', q)
+    }
+}
+
+const interval = setInterval(()=>{
+    changeTab()
+}, 1000)
+
+
+
+tabParent.onclick = (event) =>{
+    
+    if(event.target.classList.contains('tab_content_item')){
+        tabItems.forEach((item, index) =>{
+            if(event.target === item){
+                q = index
+                hideTabContent()
+                showTabContent(index)
+            }
+        })
+    }
+}
